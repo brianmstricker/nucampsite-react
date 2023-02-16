9fe540e359e44ae8dd5625e93886deb56ac423f9
Brian Stricker <brianmstricker@gmail.com>
2023-02-16 15:37:16 -0500

    comments

---
+++ src/utils/formatDate.js
@@ -0,0 +1,5 @@
+export const formatDate = (date) => {
+    return new Intl.DateTimeFormat('en-US', {
+        year: 'numeric', month: 'short', day: '2-digit'
+    }).format(new Date(Date.parse(date)))
+};
