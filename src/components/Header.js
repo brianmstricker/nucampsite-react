cf87ee27369bb4fba0293e5887813b632fd23bbe
Brian Stricker <brianmstricker@gmail.com>
2023-03-02 22:34:32 -0500

    addComment

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,44 +1,44 @@
 import { Navbar, NavbarBrand, Collapse, NavbarToggler, Nav, NavItem } from "reactstrap";
 import NucampLogo from "../app/assets/img/logo.png";
 import { useState } from "react";
-import { NavLink } from "react-router-dom"; 
+import { NavLink } from "react-router-dom";
 
 
 const Header = () => {
-    const [menuOpen, setMenuOpen] = useState(false)
+    const [menuOpen, setMenuOpen] = useState(false);
     return (
         <Navbar dark color='primary' sticky='top' expand='md'>
-          <NavbarBrand href='/' className="ms-5">
-            <img src={NucampLogo} alt="nucamp logo" className="float-start"/>
-            <h1 className="mt-2">Nucamp</h1>
-          </NavbarBrand>
-            <NavbarToggler onClick={() => setMenuOpen(!menuOpen)}/>
+            <NavbarBrand href='/' className="ms-5">
+                <img src={NucampLogo} alt="nucamp logo" className="float-start" />
+                <h1 className="mt-2">Nucamp</h1>
+            </NavbarBrand>
+            <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
             <Collapse isOpen={menuOpen} navbar>
                 <Nav className="ms-auto" navbar>
                     <NavItem>
-                    <NavLink className="nav-link" to="/">
-                        <i className="fa fa-home fa-lg"/> Home
-                    </NavLink>  
+                        <NavLink className="nav-link" to="/">
+                            <i className="fa fa-home fa-lg" /> Home
+                        </NavLink>
                     </NavItem>
                     <NavItem>
                         <NavLink className="nav-link" to="/directory">
-                            <i className="fa fa-list fa-lg"/> Directory
-                        </NavLink> 
+                            <i className="fa fa-list fa-lg" /> Directory
+                        </NavLink>
                     </NavItem>
                     <NavItem>
                         <NavLink className="nav-link" to="/about">
-                            <i className="fa fa-info fa-lg"/> About
-                        </NavLink> 
+                            <i className="fa fa-info fa-lg" /> About
+                        </NavLink>
                     </NavItem>
                     <NavItem>
                         <NavLink className="nav-link" to="/contact">
-                            <i className="fa fa-address-card fa-lg"/> Contact
-                        </NavLink> 
+                            <i className="fa fa-address-card fa-lg" /> Contact
+                        </NavLink>
                     </NavItem>
                 </Nav>
             </Collapse>
-      </Navbar>
-    )
-}
+        </Navbar>
+    );
+};
 
 export default Header;
