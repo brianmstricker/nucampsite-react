d741b77d090d5d7de8d0b9b4d2c70f3bddb34063
Brian Stricker <brianmstricker@gmail.com>
2023-03-07 23:52:13 -0500

    error and loading

---
+++ src/components/Error.js
@@ -0,0 +1,11 @@
+import { Col } from "reactstrap";
+
+const Error = ({ errMsg }) => {
+    return (
+        <div>
+            <Col><h4>{errMsg}</h4></Col>
+        </div>
+    );
+};
+
+export default Error;
